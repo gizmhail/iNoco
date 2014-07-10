dc662a8c6e88c32d4065b44a2d445758d4acc545
seop8492 <sebastien.poivre@orange.com>
2014-07-10 09:11:35 +0200

    Added credentials header

---
+++ iNoco/TestNoco/credentials.h
@@ -0,0 +1,12 @@
+//
+//  credentials.h
+//  iNoco
+//
+//  Created by Sébastien POIVRE on 10/07/2014.
+//  Copyright (c) 2014 Sébastien Poivre. All rights reserved.
+//
+
+#error Uncomment once set
+//#define nolibtv_client_id @"xxxxxxxx"
+//#define nolibtv_client_secret @"xxxxxxxxxxxxxxxxxxx"
+//#define nolibtv_redirect_uri @"http://xxxxxxxxxx"
